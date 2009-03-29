51bbdb23db8efc556320bd0e572996650e6f5616
David Lazăr <dlazar@gmail.com>
2009-03-29 20:12:28 +0200

    Adds the "Edit Language" activity.

---
+++ src/ro/undef/EditLanguageActivity.java
@@ -0,0 +1,15 @@
+package ro.undef;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditLanguageActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_language);
+    }
+}
