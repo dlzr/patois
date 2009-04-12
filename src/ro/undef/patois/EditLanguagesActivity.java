41971b62d4c548bb0e937d4e28b4090ce7099f93
David Lazăr <dlazar@gmail.com>
2009-04-12 18:44:29 +0200

    Implements EditLanguagesActivity.onCreate().

--- src/ro/undef/patois/EditLanguagesActivity.java
+++ src/ro/undef/patois/EditLanguagesActivity.java
@@ -3,14 +3,23 @@ package ro.undef.patois;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
 
 /**
  * Activity for editting the list of languages.
  */
 public class EditLanguagesActivity extends Activity {
 
+    private LinearLayout mLayout;
     private LayoutInflater mInflater;
+
     private PatoisDatabase mDb;
+    private ArrayList<Language> mLanguages;
 
     /**
      * Called when the activity is first created.
@@ -20,10 +29,39 @@ public class EditLanguagesActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         mInflater = getLayoutInflater();
+        setContentView(R.layout.edit_languages);
 
         mDb = new PatoisDatabase(this);
         mDb.open();
 
-        setContentView(R.layout.edit_languages);
+        mLanguages = mDb.getLanguages();
+
+        mLayout = (LinearLayout) findViewById(R.id.list);
+        buildViews();
+    }
+
+    private void buildViews() {
+        final LinearLayout layout = mLayout;
+        layout.removeAllViews();
+
+        for (Language language : mLanguages) {
+            layout.addView(buildViewForLanguage(language));
+        }
+
+        // Add an empty language entry for new languages.
+        layout.addView(buildViewForLanguage(null));
+    }
+
+    private View buildViewForLanguage(final Language language) {
+        View view = mInflater.inflate(R.layout.edit_language_entry, mLayout, false);
+
+        if (language != null) {
+            EditText code_text = (EditText) view.findViewById(R.id.language_code);
+            code_text.setText(language.code);
+            EditText name_text = (EditText) view.findViewById(R.id.language_name);
+            name_text.setText(language.name);
+        }
+
+        return view;
     }
 }
