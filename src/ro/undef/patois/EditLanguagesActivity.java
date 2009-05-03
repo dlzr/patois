2177d359f8cdb58ef8b8e6bffd51c6f23b377cff
David Lazăr <dlazar@gmail.com>
2009-05-03 19:30:55 +0200

    Saves the focus state of the EditLanguagesActivity.

--- src/ro/undef/patois/EditLanguagesActivity.java
+++ src/ro/undef/patois/EditLanguagesActivity.java
@@ -82,10 +82,51 @@ public class EditLanguagesActivity extends Activity {
 
     private static class LanguageEntry implements Parcelable {
         // These fields are saved in the parcel.
+
+        /**
+         * ID number of the the language row in the database.
+         */
         public int id;
+
+        /**
+         * Short code for the language (e.g., "EN" for "English").
+         */
         public String code;
+
+        /**
+         * Language name (e.g., "English").
+         */
         public String name;
+
+        /**
+         * True if the data in the LanguageEntry is different from what's
+         * stored in the database.
+         */
         public boolean modified;
+
+        /**
+         * Position of the beginning of the selection in the "code" EditText
+         * (-1 if there is no selection).
+         */
+        public int codeSelectionStart;
+
+        /**
+         * Position of the end of the selection in the "code" EditText
+         * (-1 if there is no selection).
+         */
+        public int codeSelectionEnd;
+
+        /**
+         * Position of the beginning of the selection in the "name" EditText
+         * (-1 if there is no selection).
+         */
+        public int nameSelectionStart;
+
+        /**
+         * Position of the end of the selection in the "name" EditText
+         * (-1 if there is no selection).
+         */
+        public int nameSelectionEnd;
 
         // These fields are NOT saved in the parcel.
         private EditText mCodeEditText;
@@ -96,6 +137,10 @@ public class EditLanguagesActivity extends Activity {
             this.code = code;
             this.name = name;
             this.modified = false;
+            this.codeSelectionStart = -1;
+            this.codeSelectionStart = -1;
+            this.nameSelectionEnd = -1;
+            this.nameSelectionEnd = -1;
         }
 
         public LanguageEntry() {
@@ -117,6 +162,15 @@ public class EditLanguagesActivity extends Activity {
             mCodeEditText.setText(code);
             mNameEditText.setText(name);
 
+            if (codeSelectionStart != -1 && codeSelectionEnd != -1) {
+                mCodeEditText.requestFocus();
+                mCodeEditText.setSelection(codeSelectionStart, codeSelectionEnd);
+            }
+            if (nameSelectionStart != -1 && nameSelectionEnd != -1) {
+                mNameEditText.requestFocus();
+                mNameEditText.setSelection(nameSelectionStart, nameSelectionEnd);
+            }
+
             view.setTag(this);
 
             return view;
@@ -131,6 +185,21 @@ public class EditLanguagesActivity extends Activity {
 
             code = new_code;
             name = new_name;
+
+            if (mCodeEditText.hasFocus()) {
+                codeSelectionStart = mCodeEditText.getSelectionStart();
+                codeSelectionEnd = mCodeEditText.getSelectionEnd();
+            } else {
+                codeSelectionStart = -1;
+                codeSelectionEnd = -1;
+            }
+            if (mNameEditText.hasFocus()) {
+                nameSelectionStart = mNameEditText.getSelectionStart();
+                nameSelectionEnd = mNameEditText.getSelectionEnd();
+            } else {
+                nameSelectionStart = -1;
+                nameSelectionEnd = -1;
+            }
         }
 
         // The Parcelable interface implementation.
@@ -144,6 +213,10 @@ public class EditLanguagesActivity extends Activity {
             out.writeString(code);
             out.writeString(name);
             out.writeInt(modified ? 1 : 0);
+            out.writeInt(codeSelectionStart);
+            out.writeInt(codeSelectionEnd);
+            out.writeInt(nameSelectionStart);
+            out.writeInt(nameSelectionEnd);
         }
 
         public static final Parcelable.Creator CREATOR
@@ -154,6 +227,11 @@ public class EditLanguagesActivity extends Activity {
                                                            in.readString(),
                                                            in.readString());
                 language.modified = in.readInt() == 1;
+
+                language.codeSelectionStart = in.readInt();
+                language.codeSelectionEnd = in.readInt();
+                language.nameSelectionStart = in.readInt();
+                language.nameSelectionEnd = in.readInt();
 
                 return language;
             }
