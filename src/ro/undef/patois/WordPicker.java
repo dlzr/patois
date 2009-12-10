0ee8be2742286ed3d6c372bf3d5e4ebed281db56
David Lazăr <dlazar@gmail.com>
2009-12-10 22:00:00 +0100

    Adds a stub WordPicker class, removes default text from layout.

---
+++ src/ro/undef/patois/WordPicker.java
@@ -0,0 +1,9 @@
+package ro.undef.patois;
+
+public class WordPicker {
+
+    public Word pickWord(PatoisDatabase db) {
+        // TODO: Implement me.
+        return new Word(42, "mămăligă", new Language(1, "ro", "Romanian", 1));
+    }
+}
