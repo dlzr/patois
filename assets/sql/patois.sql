3332d7094c61f08679330cd726edb15fd4d0a179
David Lazăr <dlazar@gmail.com>
2010-11-13 18:59:55 +0100

    Sets the license to the Apache License, Version 2.0.

--- assets/sql/patois.sql
+++ assets/sql/patois.sql
@@ -1,4 +1,18 @@
--- This file contains the database schema for Patois.
+-- The database schema for Patois.
+-- Copyright 2010 David Lazăr
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
 --
 -- The Java class android.database.SQLiteDatabase only only allows the
 -- execution of one SQL statement at a time.  Since we can't split a file into
